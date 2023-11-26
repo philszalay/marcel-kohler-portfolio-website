9442a4e6e8bd19dd95a05c4f4cc109c3c63fd69f
Phil Szalay <philszalay@aol.de>
2023-11-26 21:57:19 +0100

    Customer shader + shaderPass

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { DragControls } from 'three/addons/controls/DragControls.js';
+import EffectComposer, {
+  RenderPass,
+  ShaderPass,
+} from '@johh/three-effectcomposer';
 
 export default class ThreeJsDraft {
-  constructor () {
+  constructor() {
     /**
      * Variables
      */
@@ -32,11 +36,6 @@ export default class ThreeJsDraft {
     })
     this.renderer.setSize(this.width, this.height)
     this.renderer.setPixelRatio(Math.min(this.devicePixelRatio, 2))
-
-    /**
-     * Controls
-     */
-    this.orbitControls = new OrbitControls(this.camera, this.canvas)
 
     /**
      * Resize
@@ -89,41 +88,123 @@ export default class ThreeJsDraft {
      */
     this.addObjects()
 
-    this.renderer.render(this.scene, this.camera)
+    /**
+     * Event Listeners
+     */
+    this.addEventListeners();
+
     /**
      * Animation Loop
      */
     this.animate()
   }
 
-  loadAssets () {
+  addEventListeners() {
+    const mouseMoveFunction = (e) => {
+      // mousemove / touchmove
+      this.uMouse.x = (e.clientX / window.innerWidth);
+      this.uMouse.y = 1. - (e.clientY / window.innerHeight);
+    }
+
+    this.uMouse = new THREE.Vector2(0, 0)
+    this.raycaster = new THREE.Raycaster();
+    this.pointer = new THREE.Vector2();
+
+    document.addEventListener('mousedown', (e) => {
+      this.pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
+      this.pointer.y = - (e.clientY / window.innerHeight) * 2 + 1;
+
+      this.raycaster.setFromCamera(this.pointer, this.camera);
+
+      const intersects = this.raycaster.intersectObjects([this.cube]);
+
+      console.log(intersects);
+
+      if (intersects.length) {
+
+        document.addEventListener('mousemove', mouseMoveFunction);
+      }
+
+
+    });
+
+    document.addEventListener('mouseup', (e) => {
+      document.removeEventListener('mousemove', mouseMoveFunction);
+      this.uMouse.x = 0;
+      this.uMouse.y = 0;
+    });
+  }
+
+  loadAssets() {
     // const textureLoader = new THREE.TextureLoader(this.loadingManager)
   }
 
-  addHelpers () {
+  addHelpers() {
     const axisHelper = new THREE.AxesHelper(3)
-    this.scene.add(axisHelper)
+    //this.scene.add(axisHelper)
 
     this.stats = Stats()
     document.body.appendChild(this.stats.dom)
   }
 
-  addObjects () {
-    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+  addObjects() {
+    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1, 16, 16, 16)
     const cubeMaterial = new THREE.MeshBasicMaterial({
-      color: 0x00ff00
+      color: 0x00ff00,
+      wireframe: true
     })
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-    this.scene.add(cube)
+    this.cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+    this.scene.add(this.cube)
+
+    this.composer = new EffectComposer(this.renderer);
+    const renderPass = new RenderPass(this.scene, this.camera, cubeMaterial);
+    this.composer.addPass(renderPass);
+
+    var myEffect = {
+      uniforms: {
+        "tDiffuse": { value: null },
+        "resolution": { value: new THREE.Vector2(1., window.innerHeight / window.innerWidth) },
+        "uMouse": { value: new THREE.Vector2(-10, -10) },
+        "uVelo": { value: 0 },
+      },
+      vertexShader: `varying vec2 vUv;void main() {vUv = uv;gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );}`,
+      fragmentShader: `uniform float time;
+  uniform sampler2D tDiffuse;
+  uniform vec2 resolution;
+  varying vec2 vUv;
+  uniform vec2 uMouse;
+  float circle(vec2 uv, vec2 disc_center, float disc_radius, float border_size) {
+    uv -= disc_center;
+    uv*=resolution;
+    float dist = sqrt(dot(uv, uv));
+    return smoothstep(disc_radius+border_size, disc_radius-border_size, dist);
+  }
+  void main()  {
+    vec2 newUV = vUv;
+    float c = circle(vUv, uMouse, 0.0, 0.2);
+    float r = texture2D(tDiffuse, newUV.xy += c * (0.1 * .5)).x;
+    float g = texture2D(tDiffuse, newUV.xy += c * (0.1 * .525)).y;
+    float b = texture2D(tDiffuse, newUV.xy += c * (0.1 * .55)).z;
+    vec4 color = vec4(r, g, b, 1.);
+    
+    gl_FragColor = color;
+  }`
+    }
+
+    this.customPass = new ShaderPass(myEffect);
+    this.customPass.renderToScreen = true;
+    this.composer.addPass(this.customPass);
   }
 
-  animate () {
-    this.orbitControls.update()
+  animate() {
+    this.customPass.uniforms.uMouse.value = this.uMouse;
     this.stats.update()
-    this.renderer.render(this.scene, this.camera)
+    this.composer.render()
+    //this.renderer.render(this.scene, this.camera);
     window.requestAnimationFrame(this.animate.bind(this))
   }
 }
+
 
 /**
  * Create ThreeJsDraft
